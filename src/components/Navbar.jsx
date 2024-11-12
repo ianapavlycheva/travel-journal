497bc62e4f291613cc318dbf2d4c25e971672974
ianapavlycheva <62348602+ianapavlycheva@users.noreply.github.com>
2024-11-12 14:58:06 +1100

    Add data and components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+    return (
+        <nav>
+            <img src="./src/images/Globe.png" className="nav--logo" />
+            <h1>my travel journal.</h1>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/data.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    id: 1,
+    title: "Mount Fuji",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    country: "Japan",
+    googleMapsUrl: "https://maps.app.goo.gl/G9CjNResVo116zxC8",
+    coverImg: "https://scrimba.com/links/travel-journal-japan-image-url",
+    dates: {
+      arrival: "12 Jan, 2023",
+      departure: "24 Jan, 2023",
+    },
+  },
+  {
+    id: 2,
+    title: "Sydney Opera House",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+    country: "Australia",
+    googleMapsUrl: "https://maps.app.goo.gl/GbTFtTJuDkHkrQFEA",
+    coverImg: "https://scrimba.com/links/travel-journal-australia-image-url",
+    dates: {
+      arrival: "27 May, 2023",
+      departure: "8 Jun, 2023",
+    },
+  },
+  {
+    id: 3,
+    title: "Geirangerfjord",
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    country: "Norway",
+    googleMapsUrl: "https://maps.app.goo.gl/PFpRBV3SirGQyqin7",
+    coverImg: "https://scrimba.com/links/travel-journal-norway-image-url",
+    dates: {
+      arrival: "01 Oct, 2024",
+      departure: "01 Oct, 2024",
+    },
+  },
+];
