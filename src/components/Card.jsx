33311967031473db6bad7e5ba3ecf863211dd6c4
ianapavlycheva <62348602+ianapavlycheva@users.noreply.github.com>
2024-11-13 21:56:52 +1100

    Add styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,18 @@ function Card(props) {
     return (
         <div className="card">
             <img src={props.item.coverImg} className="card--image" alt={`${props.item.title}`} />
+            <div className="card--details">
+            <img className="card--logo"src="./src/images/map.png" />
             <span className="card--country">{props.item.country}</span>
             <a className="card--maps" href={props.item.googleMapsUrl} target="_blank" rel="noopener noreferrer">
                 View on Google Maps
             </a>
           <h2 className="card--title">{props.item.title}</h2>
-      <p className="card--description">{props.item.description}</p>
+
       <div className="card--dates">
         <span>{props.item.dates.arrival}</span> - <span>{props.item.dates.departure}</span>
+      </div>
+            <p className="card--description">{props.item.description}</p>
       </div>
         </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ function Navbar() {
     return (
         <nav>
             <img src="./src/images/Globe.png" className="nav--logo" />
-            <h1>my travel journal.</h1>
+            <h1 className="nav--title"> my travel journal.</h1>
         </nav>
     )
 }
